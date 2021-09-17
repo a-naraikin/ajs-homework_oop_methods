34ddcebf404d245c8c5b5e7fe427b0b940fabbc9
a-naraikin <anna.naraikin@yandex.ru>
2021-09-18 00:15:54 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ajs-homework_oop_methods
+[![Build status](https://ci.appveyor.com/api/projects/status/nj4143eewwwkw36s?svg=true)](https://ci.appveyor.com/project/a-naraikin/ajs-homework-oop-methods)
---
+++ src/js/__tests__/Swordsman.test.js
@@ -0,0 +1,12 @@
+import Swordsman from '../Swordsman';
+
+test('should return the created Swordsman', () => {
+  expect(new Swordsman('John')).toEqual({
+    name: 'John',
+    health: 100,
+    level: 1,
+    type: 'Swordsman',
+    attack: 40,
+    defence: 10,
+  });
+});
